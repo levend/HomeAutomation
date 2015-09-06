c380d692e821ae46cac1f7e55a6d45154a34411e
Szilveszter Molnar <moszi@moszi.net>
2015-09-06 15:45:00 +0200

    Add empty test projects.

---
+++ src/Gateway/HomeAutomation.XBee.Tests/XBeeFrameDeserializationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MosziNet.HomeAutomation.XBee;
+
+namespace HomeAutomation.XBee.Tests
+{
+    [TestClass]
+    public class XBeeFrameDeserializationTests
+    {
+        [TestMethod]
+        public void TestAnalogReadings()
+        {
+            byte[] buffer = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0};
+
+            XBeeFrame f = XBeeFrameSerialization.Deserialize(buffer, 18);
+
+            // Assert.IsTrue(true);
+            // TODO
+        }
+    }
+}
---
+++ test/Gateway/HomeAutomation.Sensor.Tests/LM35Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MosziNet.HomeAutomation.Sensor.Temperature;
+
+namespace HomeAutomation.Sensor.Tests
+{
+    [TestClass]
+    public class LM35Test
+    {
+        [TestMethod]
+        public void TestTemperatureFromVoltage()
+        {
+            Assert.IsTrue(LM35.TemperatureFromVoltage(350) == 35);
+        }
+    }
+}
