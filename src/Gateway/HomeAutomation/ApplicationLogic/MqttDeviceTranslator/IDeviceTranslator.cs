6838be3f5a8f7ad2d8425916481dbb899d0d9fa8
Szilveszter Molnar <moszi@moszi.net>
2015-09-24 21:40:07 +0200

    Add some comments.

--- src/Gateway/HomeAutomation/ApplicationLogic/MqttDeviceTranslator/IDeviceTranslator.cs
+++ src/Gateway/HomeAutomation/ApplicationLogic/MqttDeviceTranslator/IDeviceTranslator.cs
@@ -4,10 +4,23 @@ using MosziNet.HomeAutomation.Device;
 
 namespace MosziNet.HomeAutomation.ApplicationLogic.MqttDeviceTranslator
 {
+    /// <summary>
+    /// This interface is the bases of converting between device data and serialized messages.
+    /// </summary>
     public interface IDeviceTranslator
     {
+        /// <summary>
+        /// Serializes the device status into a string message.
+        /// </summary>
+        /// <param name="device"></param>
+        /// <returns></returns>
         string GetDeviceMessage(IDevice device);
 
+        /// <summary>
+        /// Processes the message sent to the device.
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="message"></param>
         void ProcessMessage(IDevice device, string message);
     }
 }
