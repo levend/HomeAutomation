5c59fe93815fa47f3d00b2a9efd6b7acb5ab0a90
Balazs Gollner <Balazs_Gollner@epam.com>
2016-02-19 15:26:34 +0100

    Logout functionality implemented. Layout refactored into separate templates.

--- src/Server/RuleEngineWeb/routes/auth.js
+++ src/Server/RuleEngineWeb/routes/auth.js
@@ -10,4 +10,9 @@ router.get('/login', function(req, res, next) {
 
 router.post('/login', auth.authenticate('local', { successRedirect: '/', failureRedirect: '/auth/login' }));
 
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/auth/login');
+});
+
 module.exports = router;
